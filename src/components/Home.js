3586f314ddce800b8c22deb5e1fc352174080004
Mitch Toran <mitchellht34@gmail.com>
2022-04-12 16:11:42 -0400

    picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,10 @@ function Home() {
 
     return (
         <div>
-        <h1>Welcome to My Ever Expanding Comic Database</h1>
+            <h1>Welcome to My Ever Expanding Comic Database</h1>
+            <div>
+                <img src={window.location.origin + "/logo512.png"} alt="default background for comics" />
+            </div>
         </div>
     )
 }
