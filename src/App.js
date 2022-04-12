1b3b9c5b600f411c552a4dfcbaa027e8c6cf267c
Mitch Toran <mitchellht34@gmail.com>
2022-04-12 12:47:57 -0400

    set up routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
+        <Switch>
+          <Route path='/comics'>
+            <h1>My Comics</h1>
+          </Route>
+          <Route path='/characters'>
+            <h1>Characters I know about</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Welcome to My Ever Expanding Comic Database</h1>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
