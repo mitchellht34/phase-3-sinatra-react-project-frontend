e9abc5f0381aee78358ef34173fa74b524f8037d
Mitch Toran <mitchellht34@gmail.com>
2022-04-12 16:36:14 -0400

    picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ function Home() {
         <div>
             <h1>Welcome to My Ever Expanding Comic Database</h1>
             <div>
-                <img src={window.location.origin + "/logo512.png"} alt="default background for comics" />
+                <img src={window.location.origin + "/dc_infinite_frontier_800.png"} alt="default background for comics" />
+                <div>
+                    <h3> Welcome to The Infinite Frontier!</h3>
+                </div>
             </div>
         </div>
     )
