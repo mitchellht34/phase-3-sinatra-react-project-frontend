4be13f4ade0e7cb135d5ce00898ffdd2a61c6209
Mitch Toran <mitchellht34@gmail.com>
2022-04-12 12:41:51 -0400

    set up containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
